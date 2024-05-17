16b89553850a984f9b78e2877e99c0d4bb0b864c
Ram45016 <ramu1652004@gmail.com>
2024-05-17 22:20:06 +0530

    second-commit-v2

---
+++ app/components/Container.tsx
@@ -0,0 +1,25 @@
+import { Children } from "react";
+
+interface ContainerProps{
+    children: React.ReactNode;
+}
+
+const Container:React.FC<ContainerProps> =({
+    children
+})=>{
+    return(
+        <div 
+        className="
+            max-w-screen
+            mx-auto 
+            xl:px-20 
+            md:px-10 
+            sm:px-2 
+            px-4
+        "
+    >
+        {children}
+    </div>
+    );
+}
+export default Container;
